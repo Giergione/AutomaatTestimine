f2b7af9286ef38b45e5e5783d477f7fad9f63956
kahobe <kahobe@ttu.ee>
2017-09-14 12:48:11 +0300

    k

---
+++ IlmaAPI/src/AutomatedTest1.java
@@ -0,0 +1,11 @@
+/**
+ * Created by kasutaja on 14.09.2017.
+ */
+public class AutomatedTest1 {
+    //
+    @Test
+    public void testIsTemperatureANumber()
+    {
+        assertDouble(2, myClass.add(x,y));
+    }
+}
