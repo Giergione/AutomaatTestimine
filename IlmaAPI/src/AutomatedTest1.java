4823531addbf1091b8cf32192e9ddc9bff123588
kahobe <kahobe@ttu.ee>
2017-09-27 16:52:28 +0300

    k

--- IlmaAPI/src/AutomatedTest1.java
+++ IlmaAPI/src/AutomatedTest1.java
@@ -2,24 +2,16 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by kasutaja on 14.09.2017.
- */
 public class AutomatedTest1 {
 
-    public static void main(String[] args) {
-        AutomatedTest1 automatedTest1 = new AutomatedTest1();
-        System.out.println(automatedTest1.getCoordinates());
-    }
-
-    public BigDecimal getTemperature() {
-        return null;
+    public int getTemperature() {
+        return 23;
     }
 
     public List<String> getCoordinates() {
         ArrayList<String> coordinates = new ArrayList<>(2);
-        coordinates.add(0, "testX");
-        coordinates.add(1, "testY");
+        coordinates.add(0, "12.34.56");
+        coordinates.add(1, "65.43.21");
         return coordinates;
     }
 }
--- IlmaAPI/src/AutomatedTest1Test.java
+++ IlmaAPI/src/AutomatedTest1Test.java
@@ -1,27 +1,60 @@
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 
 class AutomatedTest1Test {
 
     @Test
-    public void testTemperature() {
-
+    void CertainIntIsAchievable() {
+        int CertainInt = 55;
+        AutomatedTest1 automatedTest1 = new AutomatedTest1();
+        assertTrue(automatedTest1.getTemperature() - automatedTest1.getTemperature() + CertainInt == CertainInt);
     }
 
     @Test
-    public void testCoordinates() {
+    void higherThanMinimumTemperature() {
+        int minimumTemp = -273;
         AutomatedTest1 automatedTest1 = new AutomatedTest1();
-        ArrayList<String> toTest = new ArrayList<>();
-        toTest.add(0,"TestX");
-        toTest.add(1,"TestY");
-        assertArrayEquals(toTest, automatedTest1.getCoordinates());
+        assertTrue(automatedTest1.getTemperature() >= minimumTemp);
+    }
 
+    @Test
+    void lowerThanMaximumTemperature() {
+        int maximumTemp = 500;
+        AutomatedTest1 automatedTest1 = new AutomatedTest1();
+        assertTrue(automatedTest1.getTemperature() <= maximumTemp);
+    }
+
+    @Test
+    void checkIfTwoCoordinates() {
+        AutomatedTest1 automatedTest1 = new AutomatedTest1();
+        assertTrue(automatedTest1.getCoordinates().size() == 2);
+    }
+
+    @Test
+    void checkIfLatitudeIsInCorrectFormat() {
+        boolean areIntegers = true;
+        AutomatedTest1 automatedTest1 = new AutomatedTest1();
+        String Latitude = automatedTest1.getCoordinates().get(0);
+        String LatitudeWithoutPeriods = Latitude.replace(".", "");
+        for (int i=0; i<LatitudeWithoutPeriods.length(); i++) {
+            if (LatitudeWithoutPeriods.charAt(i) < 48 | LatitudeWithoutPeriods.charAt(i) > 57) {
+                areIntegers = false;
+            }
         }
+        assertTrue(Latitude.length() - Latitude.replace(".", "").length() == 2 & areIntegers);
+    }
+
+    @Test
+    void checkIfLongitudeIsInCorrectFormat() {
+        boolean areIntegers = true;
+        AutomatedTest1 automatedTest1 = new AutomatedTest1();
+        String Longitude = automatedTest1.getCoordinates().get(1);
+        String LongitudeWithoutPeriods = Longitude.replace(".", "");
+        for (int i=0; i<LongitudeWithoutPeriods.length(); i++) {
+            if (LongitudeWithoutPeriods.charAt(i) < 48 | LongitudeWithoutPeriods.charAt(i) > 57) {
+                areIntegers = false;
+            }
+        }
+        assertTrue(Longitude.length() - Longitude.replace(".", "").length() == 2 & areIntegers);
     }
 }
